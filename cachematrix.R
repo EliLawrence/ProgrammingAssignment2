404543817b459291b51028e94d5950367c0bf5bd
Elizabeth Lawrence <elizabeth-lawrence@hotmail.com>
2014-12-18 12:31:01 -0500

    Revert "Assignment 2 completed"
    This reverts commit 7f29401b4003ff3e45aef0a6befc20393325eead.

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,15 @@
-## The following functions work together to create matrices and 
-##solve them (i.e. get their inverse) using 
+## Put comments here that give an overall description of what your
+## functions do
 
-## Makes a special matrix object
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    m<- NULL
-    set<- function(y) {
-        x<<-y
-        m<<-NULL
-    }
-    get <- function() x
-    setsolve<- function(solve) m<<-solve
-    getsolve<- function() m
-    list(set =set, get = get,
-         setsolve = setsolve,
-         getsolve = getsolve)
+
 }
 
-#Must make an object using makeCacheMatrix to then use cacheSolve
-## Returns (or 'solves') a matrix that's the inverse of matrix 'x'
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-    m<- x$getsolve()
-    if(!is.null(m)){
-        message("getting cached data")
-        return(m)
-    }
-    data<- x$get()
-    m<- solve(data, ...)
-    x$setsolve(m)
-    m
+        ## Return a matrix that is the inverse of 'x'
 }
